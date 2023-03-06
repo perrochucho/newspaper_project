1d2f62602430581572155e4f2d584d63d61e2ac5
Luis Eladio <91117700+perrochucho@users.noreply.github.com>
2023-03-06 13:54:43 +0100

    Add files via upload

---
+++ newspaper_project/newspaper_project/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(
+        'Título',
+        max_length=200
+    )
+    author = models.ForeignKey(
+        'auth.User',
+        verbose_name='Autor',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField(
+        'Cuerpo'
+    )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
